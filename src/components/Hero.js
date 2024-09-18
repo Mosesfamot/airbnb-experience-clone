e68ccb40c41631b7cc4b447ef612f24bc9bd035e
Moses Ojo <mosesadegoke.jo@gmail.com>
2024-09-18 20:54:09 +0100

    Airbnb clone

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,9 +4,13 @@ import HeroImage from '../images/hero-image.png';
 export default function Hero() {
     return (
         <section className="container">
-            <img src={HeroImage} className="hero-image" alt="hero-image"></img>
-            <h2>Online Experinces</h2>
-            <p>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
+            <section className="hero-image">
+                <img src={HeroImage} alt="hero-image"></img>
+            </section>
+            <section className="text">
+                <h2>Online Experinces</h2>
+                <p>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
+            </section>
         </section>
     )
 }
