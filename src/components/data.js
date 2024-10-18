1f1006a46c11d469f28bfbfdffbe3f557b44d48a
Moses Ojo <mosesadegoke.jo@gmail.com>
2024-10-18 17:06:59 +0100

    Airbnb clone

--- src/components/data.js
+++ src/components/data.js
@@ -19,7 +19,7 @@ export default [
         title: "Life Lessons with Katie Zaferes",
         description: "Interested in becoming a wedding photograher? For beginner and experinced photographers alike join us in learning techniques required to leave the happy couple with memories that'll last a lifetime",
         price: 50,
-        coverImg: require('../images/image-card1.png'),
+        coverImg: require('../images/image-card2.png'),
         stats: {
             rating: 5.0,
             reviewCount: 20
@@ -32,7 +32,7 @@ export default [
         title: "Life Lessons with Katie Zaferes",
         description: "Interested in becoming a wedding photograher? For beginner and experinced photographers alike join us in learning techniques required to leave the happy couple with memories that'll last a lifetime",
         price: 20,
-        coverImg: require('../images/image-card1.png'),
+        coverImg: require('../images/image-card3.png'),
         stats: {
             rating: 5.0,
             reviewCount: 20
@@ -45,7 +45,7 @@ export default [
         title: "Life Lessons with Katie Zaferes",
         description: "Interested in becoming a wedding photograher? For beginner and experinced photographers alike join us in learning techniques required to leave the happy couple with memories that'll last a lifetime",
         price: 300,
-        coverImg: require('../images/image-card1.png'),
+        coverImg: require('../images/image-card4.png'),
         stats: {
             rating: 5.0,
             reviewCount: 20
