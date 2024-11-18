da140995e1b4e24c1dfc494cd9a03d7fdf2e6783
Moses Ojo <mosesadegoke.jo@gmail.com>
2024-11-18 16:07:11 +0100

    Airbnb clone

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,16 +12,16 @@ export default function Card(props) {
     return (
         <section className="card-section">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={props.img} className="card-image" alt="card_image"></img>
+            <img src={props.item.coverImg} className="card-image" alt="card_image"></img>
             <article className="card-rating">
                 <article className="image-span">
                     <img src={require('../images/Star1.png')} className="star-image" alt="star_image"></img>
-                    <span>{props.rating}</span>
-                    <span className="light">({props.reviewCount})</span>
-                    <span className="light">{props.location}</span>
+                    <span>{props.item.stats.rating}</span>
+                    <span className="light">({props.item.stats.reviewCount})</span>
+                    <span className="light">{props.item.location}</span>
                 </article>
-                <p>{props.title}</p>
-                <p className="price"><span>From ${props.price}</span> / person </p>
+                <p>{props.item.title}</p>
+                <p className="price"><span>From ${props.item.price}</span> / person </p>
             </article>
         </section>
     )
